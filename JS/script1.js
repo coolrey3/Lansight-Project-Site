fe23386f46713316b13905588edf979681cfc601
ray <coolrey3@gmail.com>
2019-07-03 19:39:34 -0400

    Update script1.js

--- JS/script1.js
+++ JS/script1.js
@@ -3,49 +3,51 @@ var app = express();
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-var ingredients = [
-{
-  "id":"2343sdf",
-  "text":"eggs"
-}  ,
-{
-  "id":"342342",
-  "text":"milk"
-},
-{
-  "id":"342343",
-  "text":"bacon"
-}
+var ingredients = [{
+    "id": "2343sdf",
+    "text": "eggs"
+  },
+  {
+    "id": "342342",
+    "text": "milk"
+  },
+  {
+    "id": "342343",
+    "text": "bacon"
+  }
 ];
 
-app.get ('/', function(request,response) {
-    response.send(ingredients);
+app.get('/', function(request, response) {
+  response.send(ingredients);
 });
 
-app.post('/', function(req,res){
-var ingredient = req.body;
-if (!ingredient || ingredient.text == ""){
-  res.status(500).send({error:"Your ingredient must have a name"});
-} else {
-  ingredients.push(ingredient);
-  res.status(200).send(ingredient);
-}
+app.post('/', function(req, res) {
+  var ingredient = req.body;
+  if (!ingredient || ingredient.text == "") {
+    res.status(500).send({
+      error: "Your ingredient must have a name"
+    });
+  } else {
+    ingredients.push(ingredient);
+    res.status(200).send(ingredients);
+  }
 });
 
-app.put('/:ingredientId',function(req,res) {
+app.put('/:ingredientId', function(req, res) {
 
   var ingredientId = request.params.ingredientId;
   var text = request.body.text;
 
-
 });
 
-app.get ('/skate', function(req,res) {
-    res.send('Skate or die!');
+app.get('/skate', function(req, res) {
+  res.send('Skate or die!');
 });
 
-app.listen(3000,function(){
+app.listen(3000, function() {
   console.log("First API running on port 3000!!");
 });
