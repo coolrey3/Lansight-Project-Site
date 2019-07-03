f2659a90365f5520e7cefff9731efafc865e1814
Ray Mercedes <contact@RayMercedes.com>
2019-07-03 18:42:20 -0400

    express installed and api started

--- JS/script1.js
+++ JS/script1.js
@@ -1,1 +1,43 @@
-// sript js
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+var ingredients = [
+{
+  "id":"2343sdf",
+  "text":"eggs"
+}  ,
+{
+  "id":"342342",
+  "text":"milk"
+},
+{
+  "id":"342343",
+  "text":"bacon"
+}
+];
+
+app.get ('/', function(request,response) {
+    response.send(ingredients);
+});
+
+app.post('/', function(req,res){
+var ingredient = req.body;
+if (!ingredient || ingredient.text == ""){
+  res.status(500).send({error:"Your ingredient must have a name"});
+} else {
+  ingredients.push(ingredient);
+  res.status(200).send(ingredient);
+}
+});
+
+app.get ('/skate', function(req,res) {
+    res.send('Skate or die!');
+});
+
+app.listen(3000,function(){
+  console.log("First API running on port 3000!!");
+});
